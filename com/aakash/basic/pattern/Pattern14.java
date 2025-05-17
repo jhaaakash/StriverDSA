833166b3d6b2a47a2de273c565a67b62a949de63
jhaaakash <aakashkumarjha295@gmail.com>
2025-05-17 17:07:07 +0530

    Pattern14

---
+++ com/aakash/basic/pattern/Pattern14.java
@@ -0,0 +1,27 @@
+package com.aakash.basic.pattern;
+
+public class Pattern14
+{
+    // Function to print pattern12
+    public void pattern14(int n) {
+
+        for (int i = 0; i < n; i++) {
+
+            char first = 'A';
+
+            for (char ch = 'A'; ch <= 'A' + i; ch++) {
+
+                System.out.print(ch);
+            }
+
+            System.out.println();
+        }
+    }
+
+
+    public static void main(String[] args) {
+        int N = 5;
+        Pattern14 patern = new Pattern14();
+        patern.pattern14(N);
+    }
+}
