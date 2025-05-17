c5640c745599863718d9fa33e21e21043d0bcacd
jhaaakash <aakashkumarjha295@gmail.com>
2025-05-17 09:25:14 +0530

    Pattern3

---
+++ com/aakash/basic/pattern/Pattern4.java
@@ -0,0 +1,24 @@
+package com.aakash.basic.pattern;
+
+public class Pattern4
+{
+    public void pattern4(int n) {
+        for (int i = 1; i <=n; i++) {
+
+            // Inner loop which loops for the columns.
+            for (int j = 1; j <=i; j++) {
+                System.out.print(i);
+            }
+            /* As soon as stars for each iteration are printed,
+             move to the next row and give a line break */
+            System.out.println();
+        }
+
+    }
+
+    public static void main(String[] args) {
+        int N = 5;
+        Pattern4 patern = new Pattern4();
+        patern.pattern4(N);
+    }
+}
