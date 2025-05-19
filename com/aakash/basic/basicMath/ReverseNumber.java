b5434d72af4ba58f750b049483bc86b3d1c48911
jhaaakash <aakashkumarjha295@gmail.com>
2025-05-19 22:52:53 +0530

    ReverseNumber

---
+++ com/aakash/basic/basicMath/ReverseNumber.java
@@ -0,0 +1,34 @@
+package com.aakash.basic.basicMath;
+
+public class ReverseNumber {
+
+    public int reverseNumber(int n) {
+        int revnum=0;
+
+
+        while(n>0)
+        {
+            revnum= revnum*10 +(n%10);
+            n=n/10;
+
+        }
+
+        return revnum;
+
+    }
+
+    public static void main(String[] args) {
+        int n = 6678;
+
+        /* Creating an instance of
+        Solution class */
+        ReverseNumber sol = new ReverseNumber();
+
+        // Function call to reverse the digits in n
+        int ans = sol.reverseNumber(n);
+        System.out.println("The reverse of given number is: " + ans);
+    }
+}
+
+
+}
