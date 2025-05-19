71d380065c6fcdd5e5196455bd3b9797bc5568ac
jhaaakash <aakashkumarjha295@gmail.com>
2025-05-19 22:14:34 +0530

    CountAllDigitOfNumber

--- com/aakash/basic/basicMath/CountAllDigitOfNumber.java
+++ com/aakash/basic/basicMath/CountAllDigitOfNumber.java
@@ -3,7 +3,33 @@ package com.aakash.basic.basicMath;
 import java.util.HashMap;
 
 public class CountAllDigitOfNumber {
-    public static void main(String[] args) {
 
+    public int countDigit(int n) {
+    if (n==0)
+    {
+        return 0;
+    }
+    int count=0;
+
+    while(n>0)
+    {
+        n=n/10;
+        count++;
+    }
+    return  count;
+
+    }
+
+
+    public static void main(String[] args) {
+        int n = 6678;
+
+        /* Creating an instance of
+        Solution class */
+        CountAllDigitOfNumber sol = new CountAllDigitOfNumber();
+
+        // Function call to get count of digits in n
+        int ans = sol.countDigit(n);
+        System.out.println("The count of digits in the given number is: " + ans);
     }
 }
