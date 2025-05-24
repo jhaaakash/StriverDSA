b1beb62aa1861b61ca4f3305ad5fd74fc471cadc
jhaaakash <aakashkumarjha295@gmail.com>
2025-05-25 00:49:50 +0530

    AnagramOptimalAppraoch update

--- com/aakash/basic/basicString/AnagramOptimalAppraoch.java
+++ com/aakash/basic/basicString/AnagramOptimalAppraoch.java
@@ -3,26 +3,21 @@ package com.aakash.basic.basicString;
 public class AnagramOptimalAppraoch {
 
     public boolean anagramStrings(String s, String t) {
-        if(s.length() !=t.length())
-        {
+        if (s.length() != t.length()) {
             return false;
         }
         int[] count = new int[26];
 
-        for(char c: s.toCharArray())
-        {
-            count[c -'a']++;
+        for (char c : s.toCharArray()) {
+            count[c - 'a']++;
         }
 
-        for(char c: t.toCharArray())
-        {
-            count[c -'a']++;
+        for (char c : t.toCharArray()) {
+            count[c - 'a']--;
         }
 
-        for(int i:count)
-        {
-            if(i!=0)
-            {
+        for (int i : count) {
+            if (i != 0) {
                 return false;
             }
         }
