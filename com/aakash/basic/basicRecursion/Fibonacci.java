544d88e8aacc0be4a2ddc53107b5edfddca7be8b
jhaaakash <aakashkumarjha295@gmail.com>
2025-05-27 23:20:23 +0530

    Fibonacci

---
+++ com/aakash/basic/basicRecursion/Fibonacci.java
@@ -0,0 +1,18 @@
+package com.aakash.basic.basicRecursion;
+
+public class Fibonacci {
+    public int fib(int n) {
+        // Base cases: F(0) = 0, F(1) = 1
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        // Recursive case: F(n) = F(n-1) + F(n-2)
+        return fib(n - 1) + fib(n - 2);
+    }
+
+
+    public static void main(String[] args) {
+        Fibonacci solution = new Fibonacci();
+        int n = 5; // Example input
+        System.out.println("Fibonacci number at position " + n + " is " + solution.fib(n));
+    }
+}
