7df085e3de32a78e8a751dc0bf4b9c20e051c561
jhaaakash <aakashkumarjha295@gmail.com>
2025-05-28 23:02:01 +0530

    BubbleSort update

--- com/aakash/advance/sorting/algorithm/BubbleSort.java
+++ com/aakash/advance/sorting/algorithm/BubbleSort.java
@@ -1,9 +1,41 @@
 package com.aakash.advance.sorting.algorithm;
 
+import java.util.Arrays;
+
 public class BubbleSort {
 
+    public int[] bubbleSort(int[] nums) {
+        int n=nums.length;
+
+        for(int i=n-1; i>=1; i--)
+        {
+            for(int j=0; j<i;j++)
+            {
+                if(nums[j]>nums[j+1])
+                {
+                    int temp=nums[j];
+                    nums[j]=nums[j+1];
+                    nums[j+1]=temp;
+
+                }
+            }
+        }
+return  nums;
+    }
 
     public static void main(String[] args) {
+
+        // Create an instance of solution class
+        BubbleSort solution = new BubbleSort();
+
+        int[] nums = {7, 4, 1, 5, 3};
+
+        System.out.println("Array Before Using Bubble Sort: " + Arrays.toString(nums));
+
+        // Function call for Bubble Sort
+        nums = solution.bubbleSort(nums);
+
+        System.out.println("Array After Using Bubble Sort: " + Arrays.toString(nums));
 
     }
 }
