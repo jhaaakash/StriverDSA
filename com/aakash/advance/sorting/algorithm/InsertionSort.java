d68b0f85bd70bab271966d655880514cd3fd6303
jhaaakash <aakashkumarjha295@gmail.com>
2025-05-29 22:50:41 +0530

    InsertionSort Code

--- com/aakash/advance/sorting/algorithm/InsertionSort.java
+++ com/aakash/advance/sorting/algorithm/InsertionSort.java
@@ -5,14 +5,26 @@ public class InsertionSort {
 
     public int[] insertionSort(int[] nums) {
 
-        return nums;
+        int n=nums.length;
 
+        for(int i=1;i<n;i++)
+        {
+            int j=i-1;
+            int key=nums[i];
+            while(j>=0 && nums[j]>key)
+            {
+                nums[j+1]=nums[j];
+                j--;
+            }
+            nums[j+1]=key;
+        }
+        return nums;
     }
     public static void main(String[] args) {
         // Create an instance of solution class
         InsertionSort solution = new InsertionSort();
 
-        int[] nums = {13, 46, 24, 52, 20, 9};
+        int[] nums = {13, 12, 24, 52, 20, 9};
 
         System.out.println("Before Using Insertion Sort: ");
         for (int num : nums) {
