4e9be0b4702009c3067687c588a94883de5a413d
jhaaakash <aakashkumarjha295@gmail.com>
2025-06-07 10:23:53 +0530

    RotateBy90DegreeOptimal correction

--- com/aakash/advance/arrays/faq_medium/RotateBy90DegreeOptimal.java
+++ com/aakash/advance/arrays/faq_medium/RotateBy90DegreeOptimal.java
@@ -2,38 +2,25 @@ package com.aakash.advance.arrays.faq_medium;
 
 public class RotateBy90DegreeOptimal {
 // Second approach would be transpose and reverse each row
-    //
     public void rotateMatrix(int[][] matrix) {
         int n = matrix.length;
-       // int[][] ans = new int[n][n];
-
-        for(int i=0; i<n;i++)
-        {
-            for(int j=0; j<n; j++)
-            {
-                if(i==j)
-                {
-                    continue;
-                }
-                else {
-                    matrix[i][j]=matrix[j][i];
-                }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                // Swap elements across the diagonal
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
             }
         }
-    for(int i=0; i<n ;i++)
-    {
-        for (int j = 0; j < n / 2; j++) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n / 2; j++) {
 
-            // Swap elements symmetrically
-            int temp = matrix[i][j];
-            matrix[i][j] = matrix[i][n - 1 - j];
-            matrix[i][n - 1 - j] = temp;
-
+                // Swap elements symmetrically
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n - 1 - j];
+                matrix[i][n - 1 - j] = temp;
+            }
         }
-    }
-
-
-
     }
 
     public static void main(String[] args) {
